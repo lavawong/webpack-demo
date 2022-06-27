51cdfda4bb48d4592cc71cce67ebde6154c61152
Lava Wong <lazy.false@gmail.com>
2022-06-27 14:51:14 +0800

    feat: 最小复现原型

--- src/require.test.js
+++ src/require.test.js
@@ -1,17 +1,19 @@
+import './c'
 /*! art-template@4.13.1 for browser | https://github.com/aui/art-template */
-function abc(globalThis, moduleFunc) {
-    debugger;
-
-    console.log('exports=>', typeof exports, ', module=>', typeof module, ', ret=>',"object" == typeof exports && "object" == typeof module);
-    if ("object" == typeof exports && "object" == typeof module) {
-        module.exports = moduleFunc();
-    } else if ("function" == typeof define && define.amd) {
-        define([], moduleFunc)
-    } else if ("object" == typeof exports) {
-        exports.template = moduleFunc()
-    } else {
-        globalThis.template = moduleFunc()
-    }
-    debugger
-    console.log('=>', globalThis.template);
-}
+// function abc(globalThis, moduleFunc) {
+//     debugger;
+//
+//     console.log('exports=>', typeof exports, ', module=>', typeof module, ', ret=>',"object" == typeof exports && "object" == typeof module);
+//     if ("object" == typeof exports && "object" == typeof module) {
+//         module.exports = moduleFunc();
+//     } else if ("function" == typeof define && define.amd) {
+//         define([], moduleFunc)
+//     } else if ("object" == typeof exports) {
+//         exports.template = moduleFunc()
+//     } else {
+//         globalThis.template = moduleFunc()
+//     }
+//     debugger
+//     console.log('=>', globalThis.template);
+// }
+console.log('ssss=====>');
