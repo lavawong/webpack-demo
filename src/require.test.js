8b9677976bf086fd6363399fc2e6640e19eeb211
Lava Wong <lazy.false@gmail.com>
2022-12-01 22:32:56 +0800

    add sourceType test

--- src/require.test.js
+++ src/require.test.js
@@ -1,19 +1,15 @@
-import './c'
 /*! art-template@4.13.1 for browser | https://github.com/aui/art-template */
-// function abc(globalThis, moduleFunc) {
-//     debugger;
-//
-//     console.log('exports=>', typeof exports, ', module=>', typeof module, ', ret=>',"object" == typeof exports && "object" == typeof module);
-//     if ("object" == typeof exports && "object" == typeof module) {
-//         module.exports = moduleFunc();
-//     } else if ("function" == typeof define && define.amd) {
-//         define([], moduleFunc)
-//     } else if ("object" == typeof exports) {
-//         exports.template = moduleFunc()
-//     } else {
-//         globalThis.template = moduleFunc()
-//     }
-//     debugger
-//     console.log('=>', globalThis.template);
-// }
-console.log('ssss=====>');
+function abc(globalThis, moduleFunc) {
+    // babel sourceType https://babeljs.io/docs/en/options
+    console.log('exports=>', typeof exports, ', module=>', typeof module, ', ret=>',"object" == typeof exports && "object" == typeof module);
+    if ("object" == typeof exports && "object" == typeof module) {
+        module.exports = moduleFunc();
+    } else if ("function" == typeof define && define.amd) {
+        define([], moduleFunc)
+    } else if ("object" == typeof exports) {
+        exports.template = moduleFunc()
+    } else {
+        globalThis.template = moduleFunc()
+    }
+    console.log('=>', globalThis.template);
+}
